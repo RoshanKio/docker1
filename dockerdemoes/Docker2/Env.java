168cc057283470be2fcec81738fd31423713e143
Ram <ram@krishna.com>
2023-12-28 23:30:09 -0500

    msg

---
+++ dockerdemoes/Docker2/Env.java
@@ -0,0 +1,5 @@
+public class Env {
+	public static void main(String ... tts {
+		System.out.println ("NAME : "+System.getenv("NAME"));
+	}
+}
---
+++ dockerdemoes/Docker3/DockerImage/Dt.java
@@ -0,0 +1,6 @@
+import java.time.LocalDate;
+public class Dt {
+	public static void main(String[] args) {
+		System.out.println("Today is "+LocalDate.now());
+	}
+}
